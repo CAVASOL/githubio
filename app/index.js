2e771f34aeb2cdff868e7991f1f583297d3491e6
Yeon Kim <awyeon@gmail.com>
2022-06-21 15:29:14 +0800

    battle instructions & results

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
+import Battle from './components/Battle'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div className='container'>
+        <Battle />
       </div>
     )
   }
